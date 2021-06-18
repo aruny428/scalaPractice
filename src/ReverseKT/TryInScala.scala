aa2ae489d721fc29180bcde4e7e562be410d968c
Arun Yadav <GSC-30710@GSLAB.COM>
2021-06-18 21:39:24 +0530

    added few things

--- src/ReverseKT/TryInScala.scala
+++ src/ReverseKT/TryInScala.scala
@@ -5,15 +5,22 @@ import scala.util.{Try,Success,Failure}
 
 object TryInScala extends App {
 
-    def readTextFile(filename: String): Try[List[String]] = {
-      Try(Source.fromFile(filename).getLines.toList)
+    def readTextFile(filename: String): Try[Seq[Customer]] = {
+      Try(Source.fromFile(filename).getLines.toSeq.map { l =>
+        val str = l.split(",",3)
+        Customer(str(0),str(1),str(2))
+      })
     }
 
     val filename = "C:/Users/gsc-30710/Desktop/files/topics.txt"
     readTextFile(filename) match {
-      case Success(lines) => lines.foreach(println)
+      case Success(lines) => lines.foreach(l =>
+        println(l.deviceType)
+      )
       case Failure(f) => println(f)
     }
 
 
 }
+
+case class Customer(customerId: String, deviceId: String, deviceType: String)
